c0f25ed3165fd9ad9c29a09f27b254b8ea9501d5
akirapham <vancam.pham@hotmail.com>
2025-09-20 01:41:52 +0700

    feat: add calculate_output_amount for raydium v4

--- aggregator/src/dex/mod.rs
+++ aggregator/src/dex/mod.rs
@@ -2,11 +2,11 @@ pub mod event_handler;
 // pub mod orca;
 pub mod pumpfun;
 pub mod pumpfun_swap;
-// pub mod raydium;
+pub mod raydium;
 // pub mod raydium_cpmm;
 pub mod traits;
 
 pub use event_handler::*;
 // pub use orca::OrcaDex;
-// pub use raydium::RaydiumDex;
+pub use raydium::RaydiumAmmV4Dex;
 // pub use raydium_cpmm::RaydiumCpmmDex;

415a88e92ed7935e8c41caf1921db0a8a53bc7ec
giangnamnabka <giangnam.na.bka@gmail.com>
2025-12-01 10:01:12 +0700

    add calculate_output_amount for dbc

--- aggregator-sol/src/pool_data_types/dbc.rs
+++ aggregator-sol/src/pool_data_types/dbc.rs
@@ -2,8 +2,12 @@ use crate::pool_data_types::{GetAmmConfig, PoolUpdateEventType};
 use serde::{Deserialize, Serialize};
 use solana_sdk::pubkey::Pubkey;
 use solana_streamer_sdk::streaming::event_parser::protocols::meteora_dbc::parser::DBC_PROGRAM_ID;
+pub use solana_streamer_sdk::streaming::event_parser::protocols::meteora_dbc::types::{
+    LiquidityDistributionConfig, PoolConfig, PoolFeesConfig, VolatilityTracker,
+};
 use std::str::FromStr;
 use std::sync::Arc;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct DbcPoolState {
     pub slot: u64,
@@ -59,6 +63,12 @@ pub struct DbcPoolState {
     pub creator_quote_fee: u64,
     pub liquidity_usd: f64,
     pub last_updated: u64,
+
+    // PoolConfig parameters for quote calculation
+    pub pool_config: Option<PoolConfig>,
+
+    // Volatility Tracker from VirtualPool
+    pub volatility_tracker: Option<VolatilityTracker>,
 }
 
 #[allow(dead_code)]
@@ -119,6 +129,13 @@ pub struct DbcPoolUpdate {
     pub pool_update_event_type: PoolUpdateEventType,
     pub additional_event_type: i32,
     pub last_updated: u64,
+
+    // PoolConfig parameters
+    pub is_config_update: bool,
+    pub pool_config: Option<PoolConfig>,
+
+    // Volatility Tracker
+    pub volatility_tracker: Option<VolatilityTracker>,
 }
 
 #[allow(dead_code)]
@@ -127,35 +144,178 @@ impl DbcPoolState {
         Pubkey::from_str(&DBC_PROGRAM_ID.to_string()).unwrap_or_else(|_| Pubkey::default())
     }
 
-    /// Calculate output amount for PumpFun bonding curve
+    /// Calculate output amount for DBC bonding curve
     pub fn calculate_output_amount(
         &self,
-        _input_token: &Pubkey,
-        _input_amount: u64,
+        input_token: &Pubkey,
+        input_amount: u64,
         _: Arc<dyn GetAmmConfig>,
     ) -> u64 {
-        // let is_buy = tokens_equal(input_token, &get_sol_mint());
+        // Ensure we have necessary config data
+        if self.pool_config.is_none() {
+            return 0;
+        }
+        let pool_config = self.pool_config.as_ref().unwrap();
 
-        // if is_buy {
-        //     get_buy_token_amount_from_sol_amount(
-        //         input_amount,
-        //         self.base_reserve as u128,
-        //         self.quote_reserve as u128,
-        //         self.real_base as u128,
-        //         self.real_quote as u128,
-        //         0,
-        //     )
-        // } else {
-        //     get_sell_sol_amount_from_token_amount(
-        //         input_amount,
-        //         self.base_reserve as u128,
-        //         self.quote_reserve as u128,
-        //         self.real_base as u128,
-        //         self.real_quote as u128,
-        //         0,
-        //     )
-        // }
-        0
+        // Helper to convert solana_sdk::pubkey::Pubkey to anchor_lang::prelude::Pubkey
+        let to_anchor_pubkey = |p: Pubkey| anchor_lang::prelude::Pubkey::from(p.to_bytes());
+
+        // Construct VirtualPool
+        let virtual_pool = dynamic_bonding_curve::state::VirtualPool {
+            volatility_tracker: if let Some(vt) = &self.volatility_tracker {
+                dynamic_bonding_curve::state::fee::VolatilityTracker {
+                    last_update_timestamp: vt.last_update_timestamp,
+                    padding: vt.padding,
+                    sqrt_price_reference: vt.sqrt_price_reference,
+                    volatility_accumulator: vt.volatility_accumulator,
+                    volatility_reference: vt.volatility_reference,
+                }
+            } else {
+                dynamic_bonding_curve::state::fee::VolatilityTracker::default()
+            },
+            config: to_anchor_pubkey(self.config),
+            creator: to_anchor_pubkey(self.creator),
+            base_mint: to_anchor_pubkey(self.base_mint),
+            base_vault: to_anchor_pubkey(self.base_vault),
+            quote_vault: to_anchor_pubkey(self.quote_vault),
+            base_reserve: self.base_reserve,
+            quote_reserve: self.quote_reserve,
+            protocol_base_fee: self.protocol_base_fee,
+            protocol_quote_fee: self.protocol_quote_fee,
+            partner_base_fee: self.partner_base_fee,
+            partner_quote_fee: self.partner_quote_fee,
+            sqrt_price: self.sqrt_price,
+            activation_point: self.activation_point,
+            pool_type: self.pool_type,
+            is_migrated: self.is_migrated,
+            is_partner_withdraw_surplus: self.is_partner_withdraw_surplus,
+            is_protocol_withdraw_surplus: self.is_protocol_withdraw_surplus,
+            migration_progress: self.migration_progress,
+            is_withdraw_leftover: self.is_withdraw_leftover,
+            is_creator_withdraw_surplus: self.is_creator_withdraw_surplus,
+            migration_fee_withdraw_status: self.migration_fee_withdraw_status,
+            metrics: dynamic_bonding_curve::state::PoolMetrics::default(),
+            finish_curve_timestamp: self.finish_curve_timestamp,
+            creator_base_fee: self.creator_base_fee,
+            creator_quote_fee: self.creator_quote_fee,
+            creation_fee_bits: 0,
+            _padding_0: [0; 7],
+            _padding_1: [0; 6],
+        };
+
+        // Construct PoolConfig
+        let sdk_pool_fees = dynamic_bonding_curve::state::PoolFeesConfig {
+            base_fee: dynamic_bonding_curve::state::BaseFeeConfig {
+                cliff_fee_numerator: pool_config.pool_fees.base_fee.cliff_fee_numerator,
+                second_factor: pool_config.pool_fees.base_fee.second_factor,
+                third_factor: pool_config.pool_fees.base_fee.third_factor,
+                first_factor: pool_config.pool_fees.base_fee.first_factor,
+                base_fee_mode: pool_config.pool_fees.base_fee.base_fee_mode,
+                padding_0: pool_config.pool_fees.base_fee.padding_0,
+            },
+            dynamic_fee: dynamic_bonding_curve::state::DynamicFeeConfig {
+                initialized: pool_config.pool_fees.dynamic_fee.initialized,
+                padding: pool_config.pool_fees.dynamic_fee.padding,
+                max_volatility_accumulator: pool_config
+                    .pool_fees
+                    .dynamic_fee
+                    .max_volatility_accumulator,
+                variable_fee_control: pool_config.pool_fees.dynamic_fee.variable_fee_control,
+                bin_step: pool_config.pool_fees.dynamic_fee.bin_step,
+                filter_period: pool_config.pool_fees.dynamic_fee.filter_period,
+                decay_period: pool_config.pool_fees.dynamic_fee.decay_period,
+                reduction_factor: pool_config.pool_fees.dynamic_fee.reduction_factor,
+                padding2: pool_config.pool_fees.dynamic_fee.padding2,
+                bin_step_u128: pool_config.pool_fees.dynamic_fee.bin_step_u128,
+            },
+            padding_0: pool_config.pool_fees.padding_0,
+            padding_1: pool_config.pool_fees.padding_1,
+            protocol_fee_percent: pool_config.pool_fees.protocol_fee_percent,
+            referral_fee_percent: pool_config.pool_fees.referral_fee_percent,
+        };
+
+        let mut sdk_curve =
+            [dynamic_bonding_curve::state::LiquidityDistributionConfig::default(); 20];
+        for (i, point) in pool_config.curve.iter().enumerate() {
+            if i >= 20 {
+                break;
+            }
+            sdk_curve[i] = dynamic_bonding_curve::state::LiquidityDistributionConfig {
+                sqrt_price: point.sqrt_price,
+                liquidity: point.liquidity,
+            };
+        }
+
+        let sdk_pool_config = dynamic_bonding_curve::state::PoolConfig {
+            quote_mint: to_anchor_pubkey(pool_config.quote_mint),
+            fee_claimer: to_anchor_pubkey(pool_config.fee_claimer),
+            leftover_receiver: to_anchor_pubkey(pool_config.leftover_receiver),
+            pool_fees: sdk_pool_fees,
+            collect_fee_mode: pool_config.collect_fee_mode,
+            migration_option: pool_config.migration_option,
+            activation_type: pool_config.activation_type,
+            token_decimal: pool_config.token_decimal,
+            version: pool_config.version,
+            token_type: pool_config.token_type,
+            quote_token_flag: pool_config.quote_token_flag,
+            partner_locked_lp_percentage: pool_config.partner_locked_lp_percentage,
+            partner_lp_percentage: pool_config.partner_lp_percentage,
+            creator_locked_lp_percentage: pool_config.creator_locked_lp_percentage,
+            creator_lp_percentage: pool_config.creator_lp_percentage,
+            migration_fee_option: pool_config.migration_fee_option,
+            fixed_token_supply_flag: pool_config.fixed_token_supply_flag,
+            creator_trading_fee_percentage: pool_config.creator_trading_fee_percentage,
+            token_update_authority: pool_config.token_update_authority,
+            migration_fee_percentage: pool_config.migration_fee_percentage,
+            creator_migration_fee_percentage: pool_config.creator_migration_fee_percentage,
+            _padding_0: pool_config._padding_0,
+            swap_base_amount: pool_config.swap_base_amount,
+            migration_quote_threshold: pool_config.migration_quote_threshold,
+            migration_base_threshold: pool_config.migration_base_threshold,
+            migration_sqrt_price: pool_config.migration_sqrt_price,
+            locked_vesting_config: dynamic_bonding_curve::state::LockedVestingConfig {
+                amount_per_period: pool_config.locked_vesting_config.amount_per_period,
+                cliff_duration_from_migration_time: pool_config
+                    .locked_vesting_config
+                    .cliff_duration_from_migration_time,
+                frequency: pool_config.locked_vesting_config.frequency,
+                number_of_period: pool_config.locked_vesting_config.number_of_period,
+                cliff_unlock_amount: pool_config.locked_vesting_config.cliff_unlock_amount,
+                _padding: pool_config.locked_vesting_config._padding,
+            },
+            pre_migration_token_supply: pool_config.pre_migration_token_supply,
+            post_migration_token_supply: pool_config.post_migration_token_supply,
+            migrated_collect_fee_mode: pool_config.migrated_collect_fee_mode,
+            migrated_dynamic_fee: pool_config.migrated_dynamic_fee,
+            migrated_pool_fee_bps: pool_config.migrated_pool_fee_bps,
+            _padding_1: pool_config._padding_1,
+            _padding_2: pool_config._padding_2,
+            sqrt_start_price: pool_config.sqrt_start_price,
+            curve: sdk_curve,
+        };
+
+        let swap_base_for_quote = *input_token == self.base_mint;
+
+        let current_slot = self.slot;
+        let current_timestamp = self.last_updated / 1_000_000;
+
+        let result = dynamic_bonding_curve_sdk::quote_exact_in::quote_exact_in(
+            &virtual_pool,
+            &sdk_pool_config,
+            swap_base_for_quote,
+            current_timestamp,
+            current_slot,
+            input_amount,
+            false,
+        );
+
+        match result {
+            Ok(swap_result) => swap_result.output_amount,
+            Err(e) => {
+                log::warn!("DBC quote calculation failed: {}", e);
+                0
+            }
+        }
     }
 
     pub fn calculate_token_prices(
